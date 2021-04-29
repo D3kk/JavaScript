8c9d47d67220d77a28d2fc7ab95b26ad2f38944b
D3kk <brenato13@hotmail.hu>
2021-04-29 18:50:43 +0200

    First color clock

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+function colorClock() {
+  
+var date = new Date();
+  
+var hours = date.getHours();
+  
+var minutes = date.getMinutes();
+  
+var seconds = date.getSeconds();
+  
+console.log(hours + ':' + minutes + ':' + seconds);
+if (hours <= 9) {
+   hours = '0' + hours;
+  }
+  
+if (minutes <= 9 ) {
+  minutes = '0' + minutes;
+}
+  
+if (seconds <= 9) {
+  seconds = '0' + seconds;
+}
+  
+var clockFace = hours + '–' + minutes + '–' + seconds;
+  
+document.getElementById('clock').innerHTML = clockFace;
+  
+setTimeout(function() {
+  colorClock();
+}, 1000);
+
+var hexColor = '#' + hours + minutes + seconds;
+document.body.style.background = hexColor
+}
+colorClock(document.body.style.background = '#')
